dc4634d55866f7e859746d4b95c516d421319b8d
Henri Pätsi <patsihenri@gmail.com>
2024-01-14 12:39:57 +0200

    new main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,8 +4,12 @@ var level_scene = "res://scenes/levels/level.tscn"
 
 var sfx_player
 
+var instruction_text
+
 func _ready():
 	sfx_player = get_node("AudioStreamPlayer")
+	instruction_text = get_node("InstructionText")
+	instruction_text.hide()
 
 func _on_start_button_pressed():
 	sfx_player.stream = load("res://audio/sfx/SFX_START.wav")
@@ -18,3 +22,9 @@ func _on_exit_button_pressed():
 	sfx_player.play()
 	await get_tree().create_timer(0.1).timeout
 	get_tree().quit()
+
+func _on_instruction_button_mouse_entered():
+	instruction_text.show()
+
+func _on_instruction_button_mouse_exited():
+	instruction_text.hide()
