3ea5de237df36c780c099cf7af3b8ae3e6efc817
Henri Pätsi <patsihenri@gmail.com>
2024-01-14 14:11:45 +0200

    added font for score and instructions

--- README.md
+++ README.md
@@ -13,6 +13,5 @@ Our submission to the New Year New Skills Game Jam 2024!
 #### Premade assets:
 
  - relationship icons: https://pipoya.itch.io/free-popup-emotes-pack
-
-
+ - font: codeman38 (https://www.dafont.com/lunchtime-doubly-so.font)
 
--- scripts/end_menu.gd
+++ scripts/end_menu.gd
@@ -3,6 +3,7 @@ extends Node
 var level_scene = "res://scenes/levels/level.tscn"
 
 var sfx_player
+var instructions
 
 var money_label
 var friendship_label
@@ -11,16 +12,18 @@ var lovers_label
 
 func _ready():
 	sfx_player = get_node("AudioStreamPlayer")
-	money_label = get_node("MarginContainer/VBoxContainer/ScoreLabel")
-	friendship_label = get_node("MarginContainer/VBoxContainer/VBoxContainer/FriendshipsLabel")
-	lovers_label = get_node("MarginContainer/VBoxContainer/VBoxContainer/LoversLabel")
-	enemies_label = get_node("MarginContainer/VBoxContainer/VBoxContainer/EnemiesLabel")
+	instructions = get_node("Instructions")
+	money_label = get_node("Stats/ScoreLabel")
+	friendship_label = get_node("Stats/VBoxContainer/FriendshipsLabel")
+	lovers_label = get_node("Stats/VBoxContainer/LoversLabel")
+	enemies_label = get_node("Stats/VBoxContainer/EnemiesLabel")
 	money_label.text = "You made " + str(global.money) + " $"
 	friendship_label.text = "Buds: " + str(global.friends)
 	lovers_label.text = "Best Buds: " + str(global.best_friends)
 	enemies_label.text = "Enemies: " + str(global.enemies)
+	instructions.hide()
 
-func _on_restart_button_pressed():
+func _on_start_button_pressed():
 	sfx_player.stream = load("res://audio/sfx/SFX_START.wav")
 	sfx_player.play()
 	await get_tree().create_timer(1.2).timeout
@@ -31,3 +34,9 @@ func _on_exit_button_pressed():
 	sfx_player.play()
 	await get_tree().create_timer(0.1).timeout
 	get_tree().quit()
+
+func _on_instruction_button_mouse_entered():
+	instructions.show()
+
+func _on_instruction_button_mouse_exited():
+	instructions.hide()
