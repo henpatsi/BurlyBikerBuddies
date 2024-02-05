edfb48bbd412de7e81b6a2031d1b0fe9b846abca
henpatsi <49587535+henpatsi@users.noreply.github.com>
2024-02-05 19:23:33 +0200

    side views now matches front views

--- scripts/patron_builder.gd
+++ scripts/patron_builder.gd
@@ -10,32 +10,38 @@ var patron_side_textures = []
 var accessory_odds = 0.5
 var beard_odds = 0.5
 
-func _ready():
-	load_patron_front_textures()
-	load_dir_textures(patron_side_textures, patron_side_dir)
+var type_index
+var variation_index
+var hair_style_index
+var hair_color_index 
+var has_beard
+var accessory_indexes = []
 
-func load_patron_front_textures():
-	var front_dir = DirAccess.open(patron_front_dir)
-	if (!front_dir):
-		print("Error accesssing", patron_front_dir)
+func _ready():
+	load_patron_textures(patron_front_dir, patron_front_textures)
+	load_patron_textures(patron_side_dir, patron_side_textures)
+
+func load_patron_textures(dir_str, textures_list):
+	var dir = DirAccess.open(dir_str)
+	if (!dir):
+		print("Error accesssing", dir_str)
 	var child_dir_name
-	for dir_name in front_dir.get_directories():
-		var front_texture = {}
-		child_dir_name = patron_front_dir + dir_name + "/"
-		front_texture["body"] = []
-		front_texture["head"] = []
-		front_texture["accessories"] = []
-		front_texture["beard"] = []
-		front_texture["hair"] = []
-		load_dir_textures(front_texture["body"], child_dir_name + "body/")
-		load_dir_textures(front_texture["head"], child_dir_name + "head/")
-		load_dir_textures(front_texture["accessories"], child_dir_name + "accessories/")
-		load_dir_textures(front_texture["beard"], child_dir_name + "beard/")
-		load_hair_textures(front_texture["hair"], child_dir_name + "hair/")
-		front_texture["body_outline"] = load(child_dir_name + "body_outline.png")
-		front_texture["head_outline"] = load(child_dir_name + "head_outline.png")
-		patron_front_textures.append(front_texture)
-		print(front_texture)
+	for dir_name in dir.get_directories():
+		var texture_dict = {}
+		child_dir_name = dir_str + dir_name + "/"
+		texture_dict["body"] = []
+		texture_dict["head"] = []
+		texture_dict["accessories"] = []
+		texture_dict["beard"] = []
+		texture_dict["hair"] = []
+		load_dir_textures(texture_dict["body"], child_dir_name + "body/")
+		load_dir_textures(texture_dict["head"], child_dir_name + "head/")
+		load_dir_textures(texture_dict["accessories"], child_dir_name + "accessories/")
+		load_dir_textures(texture_dict["beard"], child_dir_name + "beard/")
+		load_hair_textures(texture_dict["hair"], child_dir_name + "hair/")
+		texture_dict["body_outline"] = load(child_dir_name + "body_outline.png")
+		texture_dict["head_outline"] = load(child_dir_name + "head_outline.png")
+		textures_list.append(texture_dict)
 
 func load_hair_textures(tex_list, parent_dir_str):
 	var parent_dir = DirAccess.open(parent_dir_str)
@@ -59,31 +65,51 @@ func load_dir_textures(tex_list, dir_str):
 		
 func generate_front_textures():
 	var patron_textures = {}
-	var type_index = rng.randi_range(0, patron_front_textures.size() - 1)
+	type_index = rng.randi_range(0, patron_front_textures.size() - 1)
 	var front_texture = patron_front_textures[type_index]
-	var variation_index = rng.randi_range(0, front_texture["body"].size() - 1)
+	variation_index = rng.randi_range(0, front_texture["body"].size() - 1)
 	patron_textures["body"] = front_texture["body"][variation_index]
 	patron_textures["head"] = front_texture["head"][variation_index]
 	patron_textures["body_outline"] = front_texture["body_outline"]
 	patron_textures["head_outline"] = front_texture["head_outline"]
 	# Get hair and color, none if out of range of hairs
-	var hair_style_index = rng.randi_range(0, front_texture["hair"].size())
-	var hair_color_index = rng.randi_range(0, front_texture["hair"][0].size() - 1)
+	hair_style_index = rng.randi_range(0, front_texture["hair"].size())
+	hair_color_index = rng.randi_range(0, front_texture["hair"][0].size() - 1)
 	if (hair_style_index != front_texture["hair"].size()):
 		patron_textures["hair"] = front_texture["hair"][hair_style_index][hair_color_index]
 	else:
 		patron_textures["hair"] = null
 	# Get beard, match hair color
-	if (rng.randf_range(0, 1) <= beard_odds):
+	has_beard = rng.randf_range(0, 1) <= beard_odds
+	if has_beard:
 		patron_textures["beard"] = front_texture["beard"][hair_color_index]
 	else:
 		patron_textures["beard"] = null
 	# Get any amount of accessories
+	accessory_indexes.clear()
 	patron_textures["accessories"] = []
-	for accessory in front_texture["accessories"]:
+	for i in range(front_texture["accessories"].size()):
 		if (rng.randf_range(0, 1) <= accessory_odds):
-			patron_textures["accessories"].append(accessory)
+			patron_textures["accessories"].append(front_texture["accessories"][i])
+			accessory_indexes.append(i)
 	return patron_textures
 
-func generate_side_texture(index):
-	return patron_side_textures[0]
+func generate_side_texture():
+	var patron_textures = {}
+	var side_texture = patron_side_textures[type_index]
+	patron_textures["body"] = side_texture["body"][variation_index]
+	patron_textures["head"] = side_texture["head"][variation_index]
+	patron_textures["body_outline"] = side_texture["body_outline"]
+	patron_textures["head_outline"] = side_texture["head_outline"]
+	if (hair_style_index != side_texture["hair"].size()):
+		patron_textures["hair"] = side_texture["hair"][hair_style_index][hair_color_index]
+	else:
+		patron_textures["hair"] = null
+	if has_beard:
+		patron_textures["beard"] = side_texture["beard"][hair_color_index]
+	else:
+		patron_textures["beard"] = null
+	patron_textures["accessories"] = []
+	for i in accessory_indexes:
+		patron_textures["accessories"].append(side_texture["accessories"][i])
+	return patron_textures
--- scripts/sitting_patron.gd
+++ scripts/sitting_patron.gd
@@ -8,21 +8,44 @@ var table_position
 var stats
 var stats_tracker
 
+var patron_builder
+var textures = {}
+
 func _ready():
 	stats_tracker = get_node("/root/Level/PatronInfo/")
+	patron_builder = get_node("/root/Level/PatronSpawn/PatronBuilder")
 
 func fit_to_table():
 	table_position = stats["table"].get_free_position()
 	self.transform = table_position.transform
 	set_ordering()
+	set_texture()
 
 func set_ordering():
-	self.z_index = 0
+	self.z_index = table_position.z_index
 
-func set_texture(index):
-	var sprite = get_node("Sprite")
-	var texture = stats_tracker.generate_side_texture(index)
-	sprite.set_texture(texture)
+func set_texture():
+	textures = patron_builder.generate_side_texture()
+	var head = get_node("Head")
+	get_node("Body").set_texture(textures["body"])
+	head.set_texture(textures["head"])
+	get_node("Body/Outline").set_texture(textures["body_outline"])
+	get_node("Head/Outline").set_texture(textures["head_outline"])
+	for accessory_texture in textures["accessories"]:
+		var sprite = Sprite2D.new()
+		head.add_child(sprite)
+		sprite.z_index = 5
+		sprite.set_texture(accessory_texture)
+	if (textures["hair"] != null):
+		var sprite = Sprite2D.new()
+		head.add_child(sprite)
+		sprite.z_index = 2
+		sprite.set_texture(textures["hair"])
+	if (textures["beard"] != null):
+		var sprite = Sprite2D.new()
+		head.add_child(sprite)
+		sprite.z_index = 2
+		sprite.set_texture(textures["beard"])
 
 func set_stats(start_stats, table):
 	self.stats = start_stats
